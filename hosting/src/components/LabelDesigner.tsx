5e600aa1b2f4e12f7adfa44d6d608e13ff491133
Ricard Mallafre <rikitzzz@gmail.com>
2021-11-23 22:18:49 +0100

    fix: issue with front label not being visible
    Very often the front label would not be visible in the checkout page.
    This has been fixed by not partially updating an object, but instead
    using two different string states, and completely updating an object
    when either of the string states updates.

--- hosting/src/components/LabelDesigner.tsx
+++ hosting/src/components/LabelDesigner.tsx
@@ -107,14 +107,16 @@ export const LABEL_DIMENSIONS = { width: 380, height: 532 } as const;
 export const LabelDesigner = withStyles(styles)(
   ({ order, labelDesignRef, setLabelDesign, labels, setLabels, classes }: LabelDesignerInput) => {
     const labelDesign = labelDesignRef.current;
-    const frontLabel = createRef<HTMLDivElement>();
-    const backLabel = createRef<HTMLDivElement>();
+    const frontLabelCanvasRef = createRef<HTMLDivElement>();
+    const backLabelCanvasRef = createRef<HTMLDivElement>();
     const [frontLabelCanvas, setFrontLabelCanvas] = useState<P5 | null>(null);
     const [backLabelCanvas, setBackLabelCanvas] = useState<P5 | null>(null);
     const [hasLogo, setHasLogo] = useState(!!labelDesignRef.current.logo);
     const [coffee] = order.coffees;
     const name = coffee?.display('label') || 'ROMO BLEND';
     const weight = coffee?.display('weight') || '0.25 kg';
+    const [frontLabel, setFrontLabel] = useState('');
+    const [backLabel, setBackLabel] = useState('');
 
     const centerButton = useRef<HTMLButtonElement | null>(null);
 
@@ -246,8 +248,7 @@ export const LabelDesigner = withStyles(styles)(
         p5.line(25, 420, p5.width - 25, 420);
 
         const { canvas } = p5.get();
-        const data = canvas.toDataURL();
-        if (data !== labels.front) setLabels({ ...labels, front: data });
+        setFrontLabel(canvas.toDataURL());
       };
 
       p5.mouseDragged = () => {
@@ -291,18 +292,19 @@ export const LabelDesigner = withStyles(styles)(
         p5.text(labelDesignRef.current.description, p5.width / 2, 250);
 
         const { canvas } = p5.get();
-        const data = canvas.toDataURL();
-        if (data !== labels.back) setLabels({ ...labels, back: data });
+        setBackLabel(canvas.toDataURL());
       };
     };
 
+    useEffect(() => setLabels({ front: frontLabel, back: backLabel }), [frontLabel, backLabel]);
+
     useEffect(() => {
-      if (!frontLabel.current || !backLabel.current) return;
+      if (!frontLabelCanvasRef.current || !backLabelCanvasRef.current) return;
       frontLabelCanvas?.remove();
       backLabelCanvas?.remove();
-      setFrontLabelCanvas(new P5(frontLabelSketch, frontLabel.current));
-      setBackLabelCanvas(new P5(backLabelSketch, backLabel.current));
-    }, [frontLabel.current, backLabel.current, labelDesignRef.current.logo]);
+      setFrontLabelCanvas(new P5(frontLabelSketch, frontLabelCanvasRef.current));
+      setBackLabelCanvas(new P5(backLabelSketch, backLabelCanvasRef.current));
+    }, [frontLabelCanvasRef.current, backLabelCanvasRef.current, labelDesignRef.current.logo]);
 
     return (
       <div className={classes.container}>
@@ -457,8 +459,8 @@ export const LabelDesigner = withStyles(styles)(
             </RadioGroup>
           </FormControl>
         </div>
-        <div className={classes.label} ref={frontLabel}></div>
-        <div className={classes.label} ref={backLabel}></div>
+        <div className={classes.label} ref={frontLabelCanvasRef}></div>
+        <div className={classes.label} ref={backLabelCanvasRef}></div>
       </div>
     );
   }
